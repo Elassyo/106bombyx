8a24ecb2dbb85a077344cfb3a374db9781dba695
Arthur Melin <arthur.melin@epitech.eu>
2017-02-11 15:23:28 +0100

    Growth mode

--- bombyx.py
+++ bombyx.py
@@ -5,10 +5,41 @@
 ## Login   <arthur.melin@epitech.eu>
 ##
 ## Started on  Fri Feb 10 18:32:16 2017 Arthur Melin
-## Last update Fri Feb 10 18:51:54 2017 Arthur Melin
+## Last update Sat Feb 11 15:20:46 2017 Arthur Melin
 ##
 
 import sys
+
+def growth(args):
+    try:
+        n = int(args[1])
+        k = float(args[2])
+    except ValueError as err:
+        raise Exception('error: invalid arguments') from err
+    if n < 0:
+        raise Exception('error: negative number of first-gen individuals')
+    if not 1 <= k <= 4:
+        raise Exception('error: growth rate not between 1 and 4')
+
+    x = float(n)
+    for i in range(100):
+        print(i + 1, '%.2f' % x)
+        x = k * x * (1000 - x) / 1000
+
+    return (0)
+
+def scheme(args):
+    try:
+        n = int(args[1])
+        i0 = int(args[2])
+        i1 = int(args[3])
+    except ValueError as err:
+        raise Exception('error: invalid arguments') from err
+    if n < 0:
+        raise Exception('error: negative number of first-gen individuals')
+    if i0 > i1:
+        raise Exception('error: initial generation after final generation')
+    return (0)
 
 def usage(output):
     output.write('USAGE\n')
@@ -21,13 +52,17 @@ def usage(output):
     output.write('\ti1\tfinal generation (included)\n')
 
 def main(args):
-    if len(args) == 2 and args[1] == '-h':
-        usage(sys.stdout)
-        return (0)
-    elif len(args) == 3:
-        return (growth(args))
-    elif len(args) == 4:
-        return (scheme(args))
-    else:
-        usage(sys.stderr)
+    try:
+        if len(args) == 2 and args[1] == '-h':
+            usage(sys.stdout)
+            return (0)
+        elif len(args) == 3:
+            return (growth(args))
+        elif len(args) == 4:
+            return (scheme(args))
+        else:
+            usage(sys.stderr)
+            return (84)
+    except Exception as ex:
+        print(ex.args[0], file=sys.stderr)
         return (84)
